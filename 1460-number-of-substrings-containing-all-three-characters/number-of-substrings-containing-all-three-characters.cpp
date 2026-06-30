89b1ddb4d1186bbe9d0dbfbe1e84edb33e85ee60
Vivek Kumar <vip901it@gmail.com>
2026-07-01 00:18:22 +0530

    Time: 9 ms (50.34%) | Memory: 11 MB (57.43%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
-    bool desired(vector<int>&freq){
-        return(freq[0]>0&&freq[1]>0&&freq[2]>0 );
-    }
     int numberOfSubstrings(string s) {
-        vector<int>freq(3,0);
-        int ans=0;
-        int n=s.length();
-        int left=0,right=0;
-        while(right<n){
-            char ch=s[right];
-            freq[ch-'a']++;
-            while( desired(freq)){
-                ans+=n-right;
-                freq[s[left]-'a']--;
-                left++;
 
-            }
-            right++;
+        // v2 trying in differernt way
+        int n=s.size();
+        vector<int>pos(3,-1);
+        int ans=0;
+        for( int i=0;i<n;i++){
+            pos[s[i]-'a']=i;
+            ans+=1+min({pos[0],pos[1],pos[2]});
+
         }
         return ans;
         
