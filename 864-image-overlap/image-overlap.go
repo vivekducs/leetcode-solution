1b703af3a768e51b0fb1d612a4058d82206fdd4c
Vivek Kumar <vip901it@gmail.com>
2026-09-13 21:56:39 +0530

    Time: 2 ms (100.00%) | Memory: 5.3 MB (71.43%) - LeetSync

---
+++ 864-image-overlap/image-overlap.go
@@ -0,0 +1,31 @@
+func largestOverlap(img1 [][]int, img2 [][]int) int {
+    n := len(img1)
+    type pos struct{ x, y int }
+    var A, B []pos
+    for i := 0; i < n; i++ {
+        for j := 0; j < n; j++ {
+            if img1[i][j] == 1 {
+                A = append(A, pos{i, j})
+            }
+            if img2[i][j] == 1 {
+                B = append(B, pos{i, j})
+            }
+        }
+    }
+    cnt := make([][]int, 2*n)
+    for i := range cnt {
+        cnt[i] = make([]int, 2*n)
+    }
+    best := 0
+    for _, a := range A {
+        for _, b := range B {
+            dx := b.x - a.x + n
+            dy := b.y - a.y + n
+            cnt[dx][dy]++
+            if cnt[dx][dy] > best {
+                best = cnt[dx][dy]
+            }
+        }
+    }
+    return best
+}
